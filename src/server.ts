965cd03ac4ec9ecc9ae0525f7880b4555f6b22f2
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-07-03 03:04:11 -0300

    feat: endpoint get "/" showing README.md

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,13 @@ import corsOptions from "./configs/corsOptions.ts";
 import cookieParser from "cookie-parser";
 import credentials from "./middlewares/credentials.ts";
 import taskRouter from "./routes/tasksRoutes.ts";
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs";
+import markdownIt from "markdown-it";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(credentials);
@@ -15,10 +22,33 @@ app.use(cookieParser());
 
 app.use("/tasks", taskRouter);
 
+const publicPath = path.join(__dirname, "../");
+app.use(express.static(publicPath));
+
+const readmePath = path.join(publicPath, "README.md");
+const readmeContent = fs.readFileSync(readmePath, "utf8");
+const renderedReadme = markdownIt().render(readmeContent); // Render the Markdown using markdown-it
+
 const PORT = process.env.PORT || 5005;
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  const styledHTML = `
+    <html>
+      <head>
+        <style>
+          body {
+            color: #ccc;
+            background-color: #333;
+          }
+        </style>
+      </head>
+      <body>
+        ${renderedReadme}
+      </body>
+    </html>
+  `;
+  res.send(styledHTML);
 });
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
